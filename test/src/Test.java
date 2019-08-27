03447a3fec33b166e9a88b6b407702e7be9d4c41
lyd <2700489430@qq.com>
2019-08-28 01:26:20 +0800

    3 update

--- test/src/Test.java
+++ test/src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("commit");
         System.out.println("update");
+	System.out.println("2 update");
     }
 }
